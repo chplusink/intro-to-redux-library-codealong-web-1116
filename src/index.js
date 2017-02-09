f4a69e831f90f470fbaddbb3dbcc4ea3d679b9c2
Vic Lam <lam.vic.k@gmail.com>
2017-02-09 16:35:23 -0500

    Add logging state to reducer, and button to increment count

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { createStore } from 'redux'
 
 import shoppingListItemReducer from './reducers/shoppingListItemReducer'
-import App from './App';
-import './index.css';
+import App from './App'
+import './index.css'
 
-
+const store = createStore(shoppingListItemReducer)
 
 
 ReactDOM.render(
-
-    <App />
-
-  ,
+  <App store={store}/>,
   document.getElementById('root')
-);
+)
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,15 @@
 export default function shoppingListItemReducer(state = {items: []}, action) {
-  console.log(action)
+  console.log('Action: ', action)
+  let items
+  let proposedstate
   switch(action.type) {
     case 'GET_COUNT_OF_ITEMS':
-    let items;
-    items = state.items.concat(state.items.length + 1)
-    let proposedstate = Object.assign({}, {items: items})
-    console.log(proposedstate)
-     return proposedstate;
+      items = state.items.concat(state.items.length + 1)
+      proposedstate = Object.assign({}, {items: items})
+      console.log('Incremented State: ', proposedstate)
+      return proposedstate
     default:
-      console.log(state)
-      return state;
+      console.log('Same State: ', state)
+      return state
   }
 }
